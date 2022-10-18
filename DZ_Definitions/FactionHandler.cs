f40673e8b2f543a8cd70370d8fb6e15ea123adaa
Alan Wake <shitikov_1910@mail.ru>
2022-10-18 22:31:41 +0300

    -

--- DZ_Definitions/FactionHandler.cs
+++ DZ_Definitions/FactionHandler.cs
@@ -1,36 +1,33 @@
-﻿using System;
+﻿using static System.Text.Json.JsonSerializer;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Definitions
 {
-    public static class FileHandler
-    {
-        public class Info
-        {
-            public bool? Succ;
-            public string? Returns;
-        }
-
-        public static string Read(string path)
-        {
-            return "";
-        }
-        public static void Write(string path,string content)
-        {
-            FileStream fs = new FileStream("dsa",FileMode.Open)
-
-            if (fs.CanWrite)
-                fs.Write(byte.Parse(content));
-        }
-    }
     public static class FactionHandler
     {
-        public static string GetFactionBySteamID(string SteamID)
+        public static string FactionPath = "";
+        public static Dictionary<string,string>? GetLastTable()
         {
-            return "";
+            return Deserialize<Dictionary<string, string>>(FileHandler.Read(FactionPath));
+        }
+        public static void UpdateTable(string json)
+        {
+            FileHandler.Write(FactionPath, json);
+        }
+        public static string? GetFactionBySteamID(string SteamID)
+        {
+            return GetLastTable()?[SteamID];
+        }
+        public static string GetAll()
+        {
+            var tbl = GetLastTable();
+            string s = "";
+
+            if (tbl != null)
+                foreach(KeyValuePair<string,string> kvp in GetLastTable())
+                    s += $"{kvp.Key}    =    {kvp.Value}\n";
+
+            return s;
         }
     }
 }
---
+++ DZ_Definitions/FileHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Definitions
+{
+    public static class FileHandler
+    {
+        /*public class Info
+        {
+            public bool? Succ;
+            public string? Returns;
+        }*/
+
+        public static string Read(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static void Write(string path,string content)
+        {
+            File.WriteAllText(path, content);
+        }
+    }
+}
